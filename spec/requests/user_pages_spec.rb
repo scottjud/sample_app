a5b5b7614084e9e54a5fa0a912ed9ef84a251cf8
Scott Judson <scottjud@gmail.com>
2013-10-14 18:05:33 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,23 +4,24 @@ describe "User pages" do
 
   subject { page }
 
-  describe "signup page" do
-    before { visit signup_path }
+    describe "signup page" do
+      before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
+      it { should have_content('Sign up') }
+      it { should have_title(full_title('Sign up')) }
+    end
 
 
- 	describe "profile page" do
- 	 # Replace with code to make a user variable
-  	before { visit user_path(user) }
+   	describe "profile page" do
+   	  let(:user) { FactoryGirl.create(:user) }
+    	before { visit user_path(user) }
 
-  	it { should have_content(user.name) }
-  	it { should have_title(user.name) }
-  end
+    	it { should have_content(user.name) }
+    	it { should have_title(user.name) }
+    end
 
-describe "signup" do
+
+  describe "signup" do
 
     before { visit signup_path }
 
